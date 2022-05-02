ce042c0a72977ae57fba1754c8a81f8933b6faa4
Miguel Pérez <mig.perezm@gmail.com>
2022-05-02 13:11:26 -0400

    Mostrar horas desde la actual + 1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
     <title>Laravel</title>
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
     </link>
 </head>
 
@@ -30,7 +30,7 @@
     </div>
 
 
-    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ secure_asset('js/app.js') }}"></script>
 </body>
 
 </html>
